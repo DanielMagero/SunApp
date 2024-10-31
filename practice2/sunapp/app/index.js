ec52345692e6303ca10df36fc6f9432b7378bedb
DanielMagero <mdmagero700@gmail.com>
2024-10-31 09:09:21 +0300

    updated

--- practice2/sunapp/app/index.js
+++
@@ -1,5 +0,0 @@
-function index() {
-  return 2 + 2;
-}
-
-export default index;
--- practice2/sunapp/app/page.js
+++ practice2/sunapp/app/page.js
@@ -1,6 +1,6 @@
 function index() {
   return (
-    <body>
+    <div>
       <h1 textalign="center">The Sun App</h1>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnl-SsdZpw8cX-BdenGgYWD_SCb7gB6j7bbA&s"
@@ -10,7 +10,7 @@ function index() {
         src="Week_8/practice2/sunapp/app/images/sun1.jpeg"
         alt="sun picture"
       />
-    </body>
+    </div>
   );
 }
 
